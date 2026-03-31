c1a14333783272ac376cf3332033cb59780e3a71
EstEnderYT <endercristal22@gmail.com>
2026-03-31 18:11:13 +0300

    updated backend
    just ctrl+c, ctrl+v of main project's backend folder, except i only modified data.py since it had some weird stuff i'd never seen before

--- backend/app/services/airtable_service.py
+++ backend/app/services/airtable_service.py
@@ -1,4 +1,5 @@
 from pyairtable import Api
+import random
 
 class AirtableService:
     def __init__(self, token, base_id):
@@ -15,7 +16,25 @@ class AirtableService:
             return None
 
     def create_user_records(self, data):
-        return self.create_table_records("Users", data)
+        userInterests = []
+        newInterests = []
+        for interest in data.pop("interests"):
+            interest = interest.lower()
+            inInterests = False
+            for record in self.get_table_records("Interests"):
+                if interest in record["fields"]["label"]:
+                    userInterests.append(record["id"])
+                    inInterests = True
+                    break
+            if not inInterests:
+                new_record = self.create_table_records("Interests", {"label": interest})
+                userInterests.append(new_record["id"])
+                newInterests.append(new_record["id"])
+        data["userInterests"] = userInterests
+        new_user = self.create_table_records("Users", data)
+        for id in newInterests:
+            self.update_interest_record({"Users": new_user["id"]}, id)
+        return new_user
 
     def create_room_records(self, data):
         return self.create_table_records("Housing", data)
@@ -33,22 +52,55 @@ class AirtableService:
             # In a real app, you'd use a logger here
             print(f"Error fetching Airtable records: {e}")
             return None
-            
 
-    def get_user(self, id):
-        return self.get_table_records("Users", id)
+    def get_interest(self, id):
+        record = self.get_table_records("Interests", id)
+        record["fields"]["id"] = id
+        return record["fields"]
+
+    def get_user(self, id, user=None):
+        from .compatibility import Algoritm
+        info = self.get_table_records("Users", id)
+        fields = info["fields"]
+
+        fields["name"] = str(fields["firstName"])+" "+str(fields["lastName"])
+        fields["university"] = fields.pop("school")
+        fields["image"] = fields.pop("profile picture")
+        fields["interests"] = []
+        for id in fields["userInterests"]:
+            fields["interests"].append(self.get_interest(id).pop("Users"))
+
+        # siin peaks võrdlema kasutava kasutajaga *algoritm*
+        if user:
+            userinfo = self.get_table_records("Users", user)
+            userint = userinfo["fields"]["userInterests"]
+            fields["compatibility"] = Algoritm(self, userint, fields["userInterests"])
+        else:
+            fields["compatibility"] = 0
+
+        #fields["compatibility"] = random.randint(0,100) #if have actual *algoritm* remove this
+
+        info["fields"] = fields
+        return info
+
+    def get_owner(self, id):
+        return self.get_table_records("Owners", id)
 
     def get_room(self, id):
-        return self.get_table_records("Housing", id)
+        info = self.get_table_records("Housing", id)
+        fields = info["fields"]
+        fields["priceLabel"] = str(fields["priceValue"])+" / per month"
+        fields["OwnerName"] = self.get_owner(fields["Owners"][0])["fields"]["Name"]
+        fields["ownerAvatar"] = self.get_owner(fields["Owners"][0])["fields"]["Profile Picture (from Owners)"]
+
+        info["fields"] = fields
+        return info
 
     def get_users(self):
         return self.get_table_records("Users")
 
     def get_housing(self):
         return self.get_table_records("Housing")
-
-    def get_interests(self):
-        return self.get_table_records("Interests")
 
     #UPDATING
 
@@ -74,6 +126,9 @@ class AirtableService:
 
     def update_room_record(self, updates, record_id, replace=False, typecast=True):
         return self.update_table_records("Housing", updates, record_id, replace, typecast)
+
+    def update_interest_record(self, updates, record_id, replace=False, typecast=True):
+        return self.update_table_records("Interests", updates, record_id, replace, typecast)
 
     #DELETING
     def delete_table_records(self, table_name, id):
